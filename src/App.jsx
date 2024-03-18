71c6e8d24c404168753b7e92ccda037cda12c34d
ahmed <a.salawu@outlook.com>
2024-03-18 12:55:53 +0000

    Update:Added basic folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Projects from './components/Projects'
+import Contact from './components/Contact'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
-function App() {
-
+const App = () => {
   return (
-    <>
-      
-    </>
+    <div className='border border-[rgba(63,63,70,0.4)] mx-auto w-4/5 bg-[rgb(24,24,27)]'>
+      <Navbar />
+      <div className="border-b my-8 border-[rgba(63,63,70,0.4)] w-full"/>
+      <Hero />
+      <div className="border-b my-8 border-[rgba(63,63,70,0.4)] w-full"/>
+      <Projects />
+      <div className="border-b my-8 border-[rgba(63,63,70,0.4)] w-full"/>
+      <Contact />
+      <div className="border-b my-8 border-[rgba(63,63,70,0.4)] w-full"/>
+      <Footer />
+    </div>
   )
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">Hello world! we are back!</h1>
+    </div>
+  )
+}
+
+export default Navbar
