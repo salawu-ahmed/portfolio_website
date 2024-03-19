6e46fad9df57504da9c556f8ef852478090028be
ahmed <a.salawu@outlook.com>
2024-03-19 14:39:00 +0000

    Update:Created Hero Section(left-side)

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,12 @@ import Footer from './components/Footer'
 
 const App = () => {
   return (
-    <div className='border border-[rgba(63,63,70,0.4)] mx-auto w-4/5 bg-[rgb(24,24,27)]'>
+    <div className='px-7 w-[98%] md:w-[90%] border border-[rgba(63,63,70,0.4)] mx-auto lg:w-4/5 lg:px-16 bg-[rgb(24,24,27)]'>
       <Navbar />
-      <div className="border-b my-8 border-[rgba(63,63,70,0.4)] w-full"/>
       <Hero />
-      <div className="border-b my-8 border-[rgba(63,63,70,0.4)] w-full"/>
+      <div className="border-b my-8 border-[#3f3f4666] -mx-7 lg:-mx-16"/>
       <Projects />
-      <div className="border-b my-8 border-[rgba(63,63,70,0.4)] w-full"/>
+      <div className="border-b my-8 border-[#3f3f4666] -mx-7 lg:-mx-16"/>
       <Contact />
       <div className="border-b my-8 border-[rgba(63,63,70,0.4)] w-full"/>
       <Footer />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { MdVerified } from 'react-icons/md'
+import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa'
 
 const Hero = () => {
   return (
-    <div>
-      <h1>Hero Section</h1>
-    </div>
+    <section className='mt-12 flex'>
+      {/* left-side */}
+      <div className='grow'>
+        <div className="flex items-end gap-2">
+          <img src="/me 1.png" alt="" className='p-1 w-28 rounded-full border border-orange-400 drop-shadow-[2px_2px_40px_rgba(203, 200, 200, 0.5)_inset] mr-1' />
+          <span>
+            <MdVerified size={25} className='fill-custom-blue mb-2' />
+          </span>
+        </div>
+
+        <h1 className='text-title text-5xl leading-[3.5rem] font-extrabold my-6'>Software developer, technical writer and youtuber.</h1>
+
+        <p className='text-subtitle text-l leading-7 mb-8'>I'm Salawu Ahmed, a software developer and enterpreneur based Accra. I'm the founder and CEO of PIRPLE, where we develop technologies that empower regular people to explore space on their own terms.</p>
+        <div className='flex gap-6'>
+          <FaTwitter size={25} className='fill-subtitle hover:scale-125 transition-all duration-300'/>
+          <FaInstagram size={25} className='fill-subtitle hover:scale-125 transition-all duration-300'/>
+          <FaGithub size={25} className='fill-subtitle hover:scale-125 transition-all duration-300'/>
+          <FaLinkedin size={25} className='fill-subtitle hover:scale-125 transition-all duration-300'/>
+        </div>
+      </div>
+
+      <div className=''>animation</div>
+    </section>
   )
 }
 
