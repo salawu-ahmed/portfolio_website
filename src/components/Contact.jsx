18b7b15190a8a6bfbb43f70f95d314e61fef0cb5
ahmed <a.salawu@outlook.com>
2024-03-22 18:42:59 +0000

    Update:Created contact us section structure

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
+import { FaEnvelope } from 'react-icons/fa'
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact me Section</h1>
-    </div>
+    <section>
+      <hgroup>
+        <h1>
+          <FaEnvelope />
+          Contact me
+        </h1>
+        <p>Contact me for more information and get notified when I publish something new </p>
+      </hgroup>
+      <div className="flex">
+        <form action="">
+          <label htmlFor="email">Email Address:</label>
+          <input type="email" placeholder='Email Address' id='email'/>
+
+          <label htmlFor="">Your message:</label>
+          <textarea name="" id="message" cols="30" rows="10"></textarea>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
