a68434233470f75276dd538f6c31d3c5eeed34d1
ahmed <a.salawu@outlook.com>
2024-03-25 11:29:52 +0000

    Update:Created project filter feature

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,48 +1,49 @@
 import React, { useState } from 'react'
 import { FaArrowRight, FaGithub, FaLink } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { myProjects } from './data'
 
 const Projects = () => {
-  const [currentActive, setcurrentActive] = useState("all")
-  const myProjects = [
-    {
-      projectTitle: "VernChain",
-      projectDescription: "lorem ipsum datum decria",
-      imgPath: "",
-      projectDemo: "",
-      githubRepo: "",
-      projectShowcase: "",
-      category: ""
-    }
-  ]
-  const [filteredProjects, setfilteredProjects] = useState(myProjects)
-  
+  const [projects, setProjects] = useState(myProjects)
+  const handleFilter = (category) => {
+    // filter every project that has the category
+    const filteredProjects = myProjects.filter(item => {
+      return item.category.includes(category)
+    })
+    setProjects(filteredProjects)
+
+    // setProjects(
+    //   myProjects.filter(item => {
+    //     return item.category === category
+    //   })
+    // )
+  }
+
   return (
     <main className='flex flex-col gap-8 sm:flex-row items-start'>
       <section className='flex sm:flex-col gap-3 flex-wrap justify-center'>
-        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => setcurrentActive("all")}>All Projects</button>
-        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => setcurrentActive("html&css")}>HTML & CSS</button>
-        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => setcurrentActive("javascript")}>JavaScript</button>
-        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => setcurrentActive("react+materialui")}>React & Material UI</button>
-        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => setcurrentActive("node+express")}>Node & Express</button>
+        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => setProjects(myProjects)}>All Projects</button>
+        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => handleFilter("html&css")}>HTML & CSS</button>
+        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => handleFilter("javascript")}>JavaScript</button>
+        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => handleFilter("react+tailwind")}>React & Tailwind CSS</button>
       </section>
-      <section className='flex flex-wrap gap-4 justify-center'>
+      <section className='flex flex-grow flex-wrap gap-4 justify-center'>
         {
-          ['a', "aa", "aaa", "aaaa", "aaaaa","aaaaaa"].map(item => {
+          projects.map(item => {
             return (
-              <article key={item} className='border border-card-border/30 hover:border-custom-blue transition-all duration-300 w-64  rounded-l hover:rotate-1 hover:cursor-pointer hover:scale-105'>
+              <article key={item.id} className='border border-card-border/30 hover:border-custom-blue transition-all duration-300 w-64  rounded-l hover:rotate-1 hover:cursor-pointer hover:scale-105'>
                 <img src="/1.jpg" alt="" className='rounded-l' />
                 <div className='py-4 px-2'>
-                  <h1 className='text-title'>Landing Page 2</h1>
-                  <p className='text-subtitle text-xs my-3'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid repellendus inventore iure tempore veniam minus fugit.</p>
+                  <h1 className='text-title'>{item.projectTitle}</h1>
+                  <p className='text-subtitle text-xs my-3'>{item.projectDescription}</p>
                   <div className="flex justify-between">
                     <div className="flex gap-2">
-                      <Link to='/project-demo'>
+                      <a href={item.projectDemo} target='_blank'>
                         <FaLink size={20} className='hover:scale-125 text-subtitle hover:text-title' />
-                      </Link>
-                      <Link to='/'>
+                      </a>
+                      <a href={item.projectDemo}>
                         <FaGithub size={20} className='hover:scale-125 text-subtitle hover:text-title' />
-                      </Link>
+                      </a>
                     </div>
                     <Link to="/" className='flex items-center gap-2 text-custom-blue'>
                       more
---
+++ src/components/data.js
@@ -0,0 +1,52 @@
+export const myProjects = [
+    {
+      id: "154",
+      projectTitle: "VernChain",
+      projectDescription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid repellendus inventore iure tempore veniam minus fugit.",
+      imgPath: "",
+      projectDemo: "https://vernchain.netlify.app/",
+      githubRepo: "",
+      projectShowcase: "",
+      category: ["react+tailwind"]
+    },
+    {
+      id: "2654",
+      projectTitle: "BestEats",
+      projectDescription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid repellendus inventore iure tempore veniam minus fugit.",
+      imgPath: "",
+      projectDemo: "https://besteats-ahmed.netlify.app",
+      githubRepo: "",
+      projectShowcase: "",
+      category: ["react+tailwind"]
+    },
+    {
+      id: "8994",
+      projectTitle: "Rocketeer Helmets",
+      projectDescription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid repellendus inventore iure tempore veniam minus fugit.",
+      imgPath: "",
+      projectDemo: "https://rocketeerhelmets.netlify.app",
+      githubRepo: "",
+      projectShowcase: "",
+      category: ["html&css"]
+    },
+    {
+      id: "64866",
+      projectTitle: "Nexcent Agency",
+      projectDescription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid repellendus inventore iure tempore veniam minus fugit.",
+      imgPath: "",
+      projectDemo: "https://nextcent-as.netlify.app",
+      githubRepo: "",
+      projectShowcase: "",
+      category: ["react+tailwind"]
+    },
+    {
+      id: "684798",
+      projectTitle: "College Education Website",
+      projectDescription: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid repellendus inventore iure tempore veniam minus fugit.",
+      imgPath: "",
+      projectDemo: "https://college-website-tuts.netlify.app",
+      githubRepo: "",
+      projectShowcase: "",
+      category:  ["html&css","javascript"],
+    },
+  ]
