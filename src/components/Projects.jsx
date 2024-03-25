0d3ba3e817fa1b419c0eb65b2a7ee7ac551ae92e
ahmed <a.salawu@outlook.com>
2024-03-25 09:34:39 +0000

    Update:creating filter function

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaArrowRight, FaGithub, FaLink } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 
 const Projects = () => {
+  const [currentActive, setcurrentActive] = useState("all")
+  const myProjects = [
+    {
+      projectTitle: "VernChain",
+      projectDescription: "lorem ipsum datum decria",
+      imgPath: "",
+      projectDemo: "",
+      githubRepo: "",
+      projectShowcase: "",
+      category: ""
+    }
+  ]
+  const [filteredProjects, setfilteredProjects] = useState(myProjects)
+  
   return (
     <main className='flex flex-col gap-8 sm:flex-row items-start'>
       <section className='flex sm:flex-col gap-3 flex-wrap justify-center'>
-        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300'>All Projects</button>
-        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300'>HTML & CSS</button>
-        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300'>JavaScript</button>
-        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300'>React & Material UI</button>
-        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300'>Node & Express</button>
+        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => setcurrentActive("all")}>All Projects</button>
+        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => setcurrentActive("html&css")}>HTML & CSS</button>
+        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => setcurrentActive("javascript")}>JavaScript</button>
+        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => setcurrentActive("react+materialui")}>React & Material UI</button>
+        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => setcurrentActive("node+express")}>Node & Express</button>
       </section>
       <section className='flex flex-wrap gap-4 justify-center'>
         {
