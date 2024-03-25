19b63669e1cdc90e3940534b22b6f77f0e7857e4
ahmed <a.salawu@outlook.com>
2024-03-25 11:46:10 +0000

    Update:Hide/Show scroll button onScroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
@@ -7,6 +7,16 @@ import Footer from './components/Footer'
 import { IoIosArrowUp } from 'react-icons/io'
 
 const App = () => {
+  const [showScrollBTN, setshowScrollBTN] = useState(false)
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if(scrollY > 300) {
+        setshowScrollBTN(true)
+      } else {
+        setshowScrollBTN(false)
+      }
+    })
+  }, [])
   return (
     <div className='px-7 w-[98%] md:w-[90%] border border-[rgba(63,63,70,0.4)] mx-auto lg:w-[85%] lg:px-16 bg-[rgb(24,24,27)]' id='up'>
       <Navbar />
@@ -17,11 +27,14 @@ const App = () => {
       <Contact />
       <div className="border-b my-8 border-[#3f3f4666] -mx-7 lg:-mx-16" />
       <Footer />
-      <a href="#up">
-        <button className='bg-custom-blue text-title p-2 rounded-full fixed right-[3%] bottom-8'>
-          <IoIosArrowUp size={25} />
-        </button>
-      </a>
+      {
+        showScrollBTN &&
+        <a href="#up" className={`${showScrollBTN ? "opacity-100" : "opacity-0"} transition-opacity`}>
+          <button className='bg-custom-blue text-title p-2 rounded-full fixed right-[3%] bottom-8'>
+            <IoIosArrowUp size={25} />
+          </button>
+        </a>
+      }
     </div>
   )
 }
