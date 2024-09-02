a6cb0d9dce01136738cf234efbeab78ce29c1bfc
Ahmed <a.salawu@outlook.com>
2024-09-02 15:04:44 +0000

    added  more projects and mern button

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -31,6 +31,7 @@ const Projects = () => {
         <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => handleFilter("html&css")}>HTML & CSS</button>
         <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => handleFilter("javascript")}>JavaScript</button>
         <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => handleFilter("react+tailwind")}>React & Tailwind CSS</button>
+        <button className='w-32 sm:w-44 py-2 sm:py-3 text-center text-sm sm:text-base bg-[rgba(36,37,46,1)] border border-transparent focus:border-custom-blue opacity-50 focus:opacity-100 focus:font-bold focus:leading-2 hover:opacity-100 transition-all duration-300' onClick={() => handleFilter("mern")}>MERN</button>
       </section>
       <section className='flex flex-grow flex-wrap gap-4 justify-center'>
         <AnimatePresence>
@@ -42,6 +43,7 @@ const Projects = () => {
                   <div className='py-4 px-2'>
                     <h1 className='text-title'>{item.projectTitle}</h1>
                     <p className='text-subtitle text-xs my-3'>{item.projectDescription}</p>
+                    {/* <p>Technologies: React, Redux, Typescript</p> */}
                     <div className="flex justify-between">
                       <div className="flex gap-2">
                         <a href={item.projectDemo} target='_blank'>
